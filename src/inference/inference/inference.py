bd5afa26aabb7ba0a5d93266e21a152238a2ce39
loganhartford <logan.hartford@outloo.com>
2024-11-05 10:04:13 -0500

    pose inference working with custom msg

--- src/inference/inference/inference.py
+++ src/inference/inference/inference.py
@@ -8,13 +8,16 @@ import numpy as np
 
 from ultralytics import YOLO
 
+from custom_msgs.msg import Keypoints, Keypoint
+
 class InferenceNode(Node):
     def __init__(self):
         super().__init__("inference_node")
         self.subscription = self.create_subscription(Image, "image_data", self.image_callback, 10)
         self.led_publisher = self.create_publisher(String, "led_command", 10)
-        # self.bridge = CvBridge()
-        self.model_path = "/home/weedy/shared/weedy_ros/src/inference/inference/models/indoor_pose_ncnn_model"
+        self.keypoint_publisher = self.create_publisher(Keypoints, "keypoints", 10)
+
+        self.model_path = "/mnt/shared/weedy_ros/src/inference/inference/models/indoor_pose_ncnn_model"
         self.model = YOLO(self.model_path, task="pose")
 
     def image_callback(self, msg):
@@ -25,19 +28,28 @@ class InferenceNode(Node):
             channels = 3  # Assuming RGB or BGR
             img_data = np.frombuffer(msg.data, dtype=np.uint8).reshape(height, width, channels)
 
+            self.get_logger().info(f"Inferencing...")
+
             # Run inference
             results = self.model(img_data)
             if not results:
                 self.get_logger().info("No detections found.")
             else:
                 result = results[0]
-                
-                # Print detection details
-                for box in result.boxes:
-                    xyxy = box.xyxy[0].cpu().numpy() # This call to cpu might be causing an error
-                    conf = box.conf
-                    cls = box.cls
-                    self.get_logger().info(f"Bounding box coordinates: {xyxy}, Confidence: {conf}, Class: {cls}")
+                result.save()
+                msg = Keypoints()
+                for i, keypoint in enumerate(result.keypoints):
+                    data = keypoint.data
+                    kp = Keypoint()
+
+                    kp.flower = [round(x, 3) for x in data[i][0].tolist()]
+                    kp.base = [round(x, 3) for x in data[i][1].tolist()]
+                    kp.upper = [round(x, 3) for x in data[i][2].tolist()]
+                    kp.lower = [round(x, 3) for x in data[i][3].tolist()]
+                    msg.keypoints.append(kp)
+
+                self.keypoint_publisher.publish(msg)
+                self.get_logger().info(f"Published {len(msg.keypoints)} keypoints.")
         
         except Exception as e:
             # Publish an error message to led_command topic
